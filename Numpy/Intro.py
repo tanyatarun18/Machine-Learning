be9bd1a15c0236f78bc248c41e946a956de144f1
tanyatarun18 <tanyatarun03@gmail.com>
2025-02-03 12:46:00 +0530

    file changes

---
+++ Numpy/Intro.py
@@ -0,0 +1,8 @@
+# Numpy is a fundamental package for mathematical computing in python
+# It is a library based on C for performance. In ml and datascience, we work with large dataset which requires good performance.
+# Numpy is a n-dimensional array(np array) of homogenous datatype.
+
+# Difference B/W List and Numpy
+# 1. List is dynamic but numpy has fixed size.
+# 2. Changing size of array will delete the original array and create new array.
+# 3. In np array all elements are required to be of same data type.
--- Python_Loops/Q4.py
+++ Python_Loops/Q4.py
@@ -1,5 +1,24 @@
 #reverse a String using a loop
+#This is slicing method
 
 a = "Hello"
 
-for i in range()
+def reversed_string(a):
+    return a[::-1]
+
+print (reversed_string(a))
+
+#Using reversed() and join() function
+def reversed_string1(a):
+    return "".join(reversed(a))
+
+print (reversed_string(a))
+
+#Using loops
+def reverse_string2(a):
+    reversed_a = ""
+    for char in a:
+        reversed_a = char + reversed_a
+    return reversed_a
+
+print(reverse_string2(a))
